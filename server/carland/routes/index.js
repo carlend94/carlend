0b77ac551bd0760bc9b1ce6733cf66e0aead4b56
admin <admin@admins-Mac-mini.local>
2017-11-04 20:03:28 +0200

    add server

---
+++ server/carland/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/data', function(req, res) {
+    res.send('hello, bro');
+});
+
+
+module.exports = router;
