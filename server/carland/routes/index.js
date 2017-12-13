0518d308e2b9bb02d637de9cf5854cdac0812c2a
Yaroslav <you@example.com>
2017-12-13 23:58:47 +0200

    add fix for server

--- server/carland/routes/index.js
+++ server/carland/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var Bear = require('../models/bear');
+// var Bear = require('../models/bear');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,32 +12,32 @@ router.get('/lala', function(req, res, next) {
     res.send('hello')
 });
 
-router.route('/add/post').post(function (req, res) {
-    var machine = new Bear();
-    machine.name = req.body.name;
-
-    machine.save()
-        .then(item => {
-          console.log(item);
-        res.redirect('/');
-})
-.catch(err => {
-        res.status(400).send("unable to save to database");
-});
-});
-
-
-router.route('/lalala').get(function (req, res) {
-
-    Bear.find(function (err, itms){
-        if(err){
-            console.log(err);
-        }
-        else {
-            res.send(itms);
-        }
-    });
-
-});
+// router.route('/add/post').post(function (req, res) {
+//     var machine = new Bear();
+//     machine.name = req.body.name;
+//
+//     machine.save()
+//         .then(item => {
+//           console.log(item);
+//         res.redirect('/');
+// })
+// .catch(err => {
+//         res.status(400).send("unable to save to database");
+// });
+// });
+//
+//
+// router.route('/lalala').get(function (req, res) {
+//
+//     Bear.find(function (err, itms){
+//         if(err){
+//             console.log(err);
+//         }
+//         else {
+//             res.send(itms);
+//         }
+//     });
+//
+// });
 
 module.exports = router;
